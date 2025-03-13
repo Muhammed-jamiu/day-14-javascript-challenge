7b8c9cf4c5699f6a2aa75233f85814a6c1723efd
muhammed-jamiu <anatemuhammed@gmail.com>
2025-03-13 13:52:46 +0100

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("Hdllo!,Welcome to day-14 of 30-days-javascript-challege");
+try {
+    let lastName = 'Yetayeh',
+    let fullName = fistName + ' ' + lastName
+} catch (err) { console.log(err) }
+
+
+try {
+    let lastName = 'Yfhgyfh',
+        let fulname = fistName + ' ' + lastName
+} catch (err) { console.error(err) }
+// we can use console.log() or console.error() } finally { console.log('In any case I will be executed') }
+
+//
+
+    let firstName = 'Asabeneh';
+    let fulName = firstName + ' ' + lastName
+    console.log(fulName)
+    
+// Uncaught ReferenceError: lastName is not defined at <anonymous>:2:35
+    let square = 2 * 2;
+    console.log(square)
+console.log('Hello, world')
+// Uncaught SyntaxError: Unexpected identifier
+
+    let num = 10;
+    console.log(num.toLowerCase())
